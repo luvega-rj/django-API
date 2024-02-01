1fd9503d2c6f7c6d18383a2278d0cd24e5329df0
luvega-rj <luvegaronny@gmail.com>
2024-02-01 14:59:55 +0300

    post and get cruds

--- jifunze/jifunze/views.py
+++ jifunze/jifunze/views.py
@@ -1,12 +1,27 @@
 from django.http import JsonResponse
 from .models import Jifunze
 from .serializers import JifunzeSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+
+#introduce a decorator
+@api_view(['GET', 'POST'])
+def jifunze_list(request): 
 
 #get all the jifunze drinks
 #serializer them
 #return json
-def jifunze_list(request):
-
+ if request.method == 'GET':
     jifunze = Jifunze.objects.all()
     serializer = JifunzeSerializer(jifunze, many=True)
-    return JsonResponse({"drinks": serializer.data}, safe=False)
+    return JsonResponse({"jifunze": serializer.data})
+ 
+ if request.method == 'POST':
+    serializer = JifunzeSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+   
+   
